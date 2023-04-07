a5ca7876d3d53d916e9d44015e7e900f68088672
UslanA <ademuslan@gmail.com>
2023-04-07 09:46:16 +0300

    070403a

---
+++ src/Ders01/C01.java
@@ -0,0 +1,10 @@
+package Ders01;
+
+public class C01 {
+    public static void main(String[] args) {
+        /*
+       silinen bir intelijj klasörünü githup'dan nasıl getireceğimi öğreniyorum.
+
+         */
+    }
+}
