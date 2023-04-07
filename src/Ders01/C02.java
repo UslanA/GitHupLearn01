49281211b434b542efebb788027646e321eae887
UslanA <ademuslan@gmail.com>
2023-04-07 10:01:30 +0300

    070403b

--- src/Ders01/C02.java
+++ src/Ders01/C02.java
@@ -3,6 +3,8 @@ package Ders01;
 public class C02 {
     public static void main(String[] args) {
         System.out.println("hadi çöz şunu");
+        System.out.println("bu işide hallettik");
+
 
     }
 }
---
+++ src/Ders01/C03.java
@@ -0,0 +1,5 @@
+package Ders01;
+
+public class C03 {
+    //gitti gitti gari
+}
